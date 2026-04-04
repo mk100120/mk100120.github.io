c6beb82541b72381a73237db42fc478918494f2a
I543200 <kenny.ma01@sap.com>
2026-04-04 20:22:59 +0800

    redirect to selfPage repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://example.github.io',
+  site: 'https://mk100120.github.io',
+  base: '/selfPage',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
